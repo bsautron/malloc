2902d79a13709f92fe9d5c929996024e125784c0
Bruno SAUTRON <bsautron@e1r9p11.42.fr>
2016-05-12 19:13:59 +0200

    add main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <malloc.h>
+
+int main(void)
+{
+	// malloc(42);
+	// malloc(15965);
+	// malloc(84);
+	// malloc(3450);
+	// malloc(1);
+	// malloc(99857257);
+	malloc(11112447);
+	show_alloc_mem();
+	return 0;
+}
